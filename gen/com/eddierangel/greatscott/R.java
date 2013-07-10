0301a406e6992f39c110b90af3090958665793c8
erangeljr <hacker@eddierangel.com>
2013-07-09 22:33:15 -0700

    Creating TextViews

--- gen/com/eddierangel/greatscott/R.java
+++ gen/com/eddierangel/greatscott/R.java
@@ -24,14 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f08000c;
+        public static final int billEditView=0x7f080003;
+        public static final int billTextView=0x7f080002;
+        public static final int fifteenTextView=0x7f080006;
         public static final int tableLayout=0x7f080000;
         public static final int tableRow0=0x7f080001;
-        public static final int tableRow1=0x7f080002;
-        public static final int tableRow2=0x7f080003;
-        public static final int tableRow3=0x7f080004;
-        public static final int tableRow4=0x7f080005;
-        public static final int tableRow5=0x7f080006;
+        public static final int tableRow1=0x7f080004;
+        public static final int tableRow2=0x7f080008;
+        public static final int tableRow3=0x7f080009;
+        public static final int tableRow4=0x7f08000a;
+        public static final int tableRow5=0x7f08000b;
+        public static final int tenTextView=0x7f080005;
+        public static final int twentyTextView=0x7f080007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -42,7 +47,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int billTotal=0x7f050003;
+        public static final int fifteenPercent=0x7f050006;
         public static final int hello_world=0x7f050002;
+        public static final int tenPercent=0x7f050005;
+        public static final int twentyPercent=0x7f050007;
+        public static final int zeroDotZero=0x7f050004;
     }
     public static final class style {
         /** 
